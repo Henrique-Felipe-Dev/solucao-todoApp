44e06e805339a732de32674fe02ec674ec64030d
Henrique Felipe <henriquefelipe.saga@gmail.com>
2022-06-10 12:53:29 -0300

    Implementado a ViewModel compartilhada e o Hilt

--- app/src/main/java/com/generation/todoandroid/FormFragment.kt
+++ app/src/main/java/com/generation/todoandroid/FormFragment.kt
@@ -1,11 +1,13 @@
 package com.generation.todoandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.generation.todoandroid.databinding.FragmentFormBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -13,6 +15,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class FormFragment : Fragment() {
 
     private lateinit var binding: FragmentFormBinding
+    private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +23,10 @@ class FormFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentFormBinding.inflate(layoutInflater, container, false)
+
+        mainViewModel.myCategoriaResponse.observe(viewLifecycleOwner) {
+            Log.d("Requisicao", it.body().toString())
+        }
 
         binding.buttonSalvar.setOnClickListener {
             findNavController().navigate(R.id.action_formFragment_to_listFragment)
--- app/src/main/java/com/generation/todoandroid/MainViewModel.kt
+++ app/src/main/java/com/generation/todoandroid/MainViewModel.kt
@@ -1,17 +1,21 @@
 package com.generation.todoandroid
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.generation.todoandroid.api.Repository
 import com.generation.todoandroid.model.Categoria
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import retrofit2.Response
+import javax.inject.Inject
 
-class MainViewModel : ViewModel(){
-
-    val repository = Repository()
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    val repository: Repository
+        ): ViewModel(){
 
     private var _myCategoriaResponse =
         MutableLiveData<Response<List<Categoria>>>()
@@ -19,9 +23,18 @@ class MainViewModel : ViewModel(){
     val myCategoriaResponse: LiveData<Response<List<Categoria>>> =
         _myCategoriaResponse
 
+    init {
+        listCategoria()
+    }
+
     fun listCategoria(){
-        viewModelScope.launch {
-            _myCategoriaResponse.value = repository.listCategoria()
+        viewModelScope.launch{
+            try {
+                val response = repository.listCategoria()
+                _myCategoriaResponse.value = response
+            }catch (e: Exception){
+                Log.d("Erro", e.message.toString())
+            }
         }
     }
 
